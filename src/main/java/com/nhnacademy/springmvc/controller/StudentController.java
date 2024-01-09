9f261a7439585a605d35aa05ac529260f731c5bd
Daeyoul-Yang <cnrrnehfdl03@naver.com>
2024-01-10 02:00:43 +0900

    studentView

--- src/main/java/com/nhnacademy/springmvc/controller/StudentController.java
+++ src/main/java/com/nhnacademy/springmvc/controller/StudentController.java
@@ -1,11 +1,16 @@
 package com.nhnacademy.springmvc.controller;
 
 import com.nhnacademy.springmvc.domain.Student;
+import com.nhnacademy.springmvc.exception.StudentNotFoundException;
 import com.nhnacademy.springmvc.repository.StudentRepository;
+import java.util.Objects;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/student")
@@ -13,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class StudentController {
 
     private final StudentRepository studentRepository;
+    private final String YES ="yes";
 
     public StudentController(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
@@ -21,6 +27,27 @@ public class StudentController {
     @ModelAttribute("student")
     public Student getStudent(@PathVariable("studentId") Long studentId){
         Student student = studentRepository.getStudent(studentId);
+
+        if(Objects.isNull(student)){
+            throw new StudentNotFoundException();
+        }
         return student;
+
     }
+
+    @GetMapping("/{studentId}")
+    public String viewStudent(@ModelAttribute Student student, @RequestParam(name="hideScore", required = false) String hideScore , Model model){
+    if(YES.equals(hideScore)) {
+
+            model.addAttribute("hideScore", true);
+        }
+        model.addAttribute("student",student);
+
+        return "studentView";
+    }
+
+    @GetMapping("{studentId}/modify")
+    public String stdentModifyForm(){return "studentModify";}
+
+
 }
--- src/main/webapp/WEB-INF/view/studentView.jsp
+++ src/main/webapp/WEB-INF/view/studentView.jsp
@@ -1,4 +1,5 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
     <meta charset="UTF-8" />
@@ -7,7 +8,10 @@
 <body>
     이름: ${student.name}<br />
     이메일: ${student.email}<br />
-    성적: ${student.score}<br />
+    <c:if test="${!hideScore}">
+        성적: ${student.score}<br />
+    </c:if>
+
     평가: ${student.comment}<br />
     <br />
     <a href="/student/${student.id}/modify">정보 수정</a><br />
