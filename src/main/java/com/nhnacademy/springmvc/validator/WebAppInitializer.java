b98372fdeb3feef6afe6924360990e319f61dab3
Daeyoul-Yang <cnrrnehfdl03@naver.com>
2024-01-09 09:22:10 +0900

    web initializer

---
+++ src/main/java/com/nhnacademy/springmvc/validator/WebAppInitializer.java
@@ -0,0 +1,17 @@
+package com.nhnacademy.springmvc.validator;
+
+import com.nhnacademy.springmvc.config.RootConfig;
+import com.nhnacademy.springmvc.config.WebConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected String[] getServletMappings(){ return  new String[] {"/"};}
+
+    @Override
+    protected Class<?>[] getRootConfigClasses(){ return new Class[] {RootConfig.class};}
+
+    @Override
+    protected Class<?>[] getServletConfigClasses(){ return new Class[] {WebConfig.class};}
+}
